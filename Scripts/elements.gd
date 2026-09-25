3cbc5cb47270caa7864eb3169411a5177947e308
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-26 11:32:44 +1200

    Rah

---
+++ Scripts/elements.gd
@@ -0,0 +1,28 @@
+extends OmniLight3D
+class_name Elements
+
+enum Colours {
+	R,
+	G,
+	B
+}
+
+enum GameElements {
+	SULFUR, # R
+	HYDROGEN, # G
+	OXYGEN # B
+}
+
+class Element:
+	var element: GameElements
+	var colour: Colours
+
+	func _init(_element, _colour):
+		element = _element
+		colour = _colour
+
+var elements: Array[Element] = [
+	Element.new(GameElements.SULFUR, Colours.R),
+	Element.new(GameElements.HYDROGEN, Colours.G),
+	Element.new(GameElements.OXYGEN, Colours.B)
+]
