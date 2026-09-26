d569a920e3fdff6492791c41933a433b81b168ff
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-26 13:43:06 +1200

    Rah

--- Scripts/particle.gd
+++ Scripts/particle.gd
@@ -15,8 +15,8 @@ var luminosity: float
 var particle_elements: Dictionary[GameElements, ParticleElement]
 var surface_material = null
 
-var density_range = [0.2, 0.7]
-var max_size = 12
+var density_range = [0.0, 1]
+var size_range = [8, 24]
 
 func _ready() -> void:
 	randomize()
@@ -28,7 +28,7 @@ func _ready() -> void:
 
 		set_surface_override_material(0, unique_mat)
 
-	size = randf_range(0, max_size)
+	size = randf_range(size_range[0], size_range[1])
 	density = randf_range(density_range[0], density_range[1])
 	scale.x = size
 	scale.y = size
