741bc7586d8949919444b8e16b04610c2ea01529
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-26 13:26:10 +1200

    They were all yerrow

--- Scripts/particle_manager.gd
+++ Scripts/particle_manager.gd
@@ -1,2 +1,16 @@
 extends Node3D
 class_name ParticleManager
+
+@export var particle_scene: PackedScene
+@export var initial_range: int = 4000
+@export var initial_distance: int = 3000
+
+func generate():
+	for x in range(initial_range):
+		var instantiated = particle_scene.instantiate()
+
+		instantiated.position.x = randi_range(-initial_distance, initial_distance)
+		instantiated.position.y = randi_range(-initial_distance, initial_distance)
+		instantiated.position.z = randi_range(-initial_distance, initial_distance)
+
+		add_child(instantiated)
